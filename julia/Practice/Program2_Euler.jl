0b0c49fc8ce7d1d2500238ae0ddacbda60694275
vbsteja <vbsteja2@gmail.com>
2016-04-30 18:20:19 +0530

    added julia folder

---
+++ julia/Practice/Program2_Euler.jl
@@ -0,0 +1,15 @@
+
+function Hellothere()
+  println("hello")
+  x=1
+  if x==1
+    print("thuis is one")
+
+    else
+      println("so you are not one....")
+  end
+end
+function get_fib()
+  println("Hello world this is a sample program in julia");
+end
+Hellothere()
