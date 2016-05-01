06245fe254149fbdbb6b0704c4d727ec9a7f84f7
vbsteja <vbsteja2@gmail.com>
2016-05-01 20:51:57 +0530

    added scheme folder

--- clojure/my-app/project.clj
+++ clojure/my-app/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-  				[jline "2.11"]]
+                 [jline "2.11"]]
   :main ^:skip-aot my-app.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
