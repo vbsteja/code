25cbed6d9720056e76ab11f9a93804a4087be459
vbsteja <vbsteja2@gmail.com>
2016-06-07 22:38:19 +0530

    yes

--- elixir/RobotProcess.exs
+++ elixir/RobotProcess.exs
@@ -6,7 +6,23 @@ defmodule RobotProcess do
         end
 
     end
+    def fact_tail_opt(n) do
+        _fact(n,1)
+    end
+    defp _fact(0,acc) do
+         acc
+    end
+    defp _fact(n,acc) do
+        _fact(n-1,acc*n)
+    end
+    def fact(0) do
+        1
+    end
+    def fact(n) do
+        n*fact(n-1)
+    end
 end
+
 pid=spawn(RobotProcess,:greet,[])
 send pid, {self,"surya!"}
 receive do
