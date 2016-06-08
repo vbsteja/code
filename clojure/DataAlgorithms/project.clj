1184f99720cdcb49fc8ba13bd0868e00fc9e6545
vbsteja <vbsteja2@gmail.com>
2016-06-08 12:29:08 +0530

    clojure

---
+++ clojure/DataAlgorithms/project.clj
@@ -0,0 +1,10 @@
+(defproject dataalogithms "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojars.hozumi/clj-commons-exec "1.0.6"]
+                 [facts/speech-synthesis "1.0.0"]]
+
+  :main dataalogithms.core)
